cde151c40be1ea3621d97ab087003498c321e8ab
JustNU <ivancool108@gmail.com>
2025-05-12 02:03:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/notesapp/CardViewModel.java
@@ -0,0 +1,27 @@
+package com.example.notesapp;
+
+public class CardViewModel {
+    String cardViewTitle;
+    String cardViewInner;
+
+    public CardViewModel(String cardViewInner, String cardViewTitle) {
+        this.cardViewInner = cardViewInner;
+        this.cardViewTitle = cardViewTitle;
+    }
+
+    public String getCardViewTitle() {
+        return cardViewTitle;
+    }
+
+    public void setCardViewTitle(String cardViewTitle) {
+        this.cardViewTitle = cardViewTitle;
+    }
+
+    public String getCardViewInner() {
+        return cardViewInner;
+    }
+
+    public void setCardViewInner(String cardViewInner) {
+        this.cardViewInner = cardViewInner;
+    }
+}
